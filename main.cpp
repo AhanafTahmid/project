52ed34ed5fc8e40932d871d9697b212e52d4d894
AhanafTahmid <ahanaftahmid55@gmail.com>
2023-06-19 15:20:04 +0600

    .

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//https://github.com/IsratJahanR/Typing-game-project-with-cpp
+#include "function.h"
+#include <graphics.h>
+//#include <bits/stdc++.h>
+//#include <stdlib.h>
+//using namespace std;
+
+int main()
+{
+
+    //height and weight of main window
+    //width=800 and height=520
+
+    initwindow(800, 520);
+
+    login();//getting from function.h
+
+    getch();//clear the unnecessary things
+
+    return 0;
+}
